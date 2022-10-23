a9db46f3f1e9a207e91af2e7799e103c3d9c8b8e
Bernhard Haumacher <haui@haumacher.de>
2022-10-23 09:25:25 +0200

    Ticket #7: Added `module-info.java` for better controlling dependencies.

---
+++ phoneblock/src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Bernhard Haumacher et al. All Rights Reserved.
+ */
+
+/**
+ * The PhoneBlock application.
+ */
+module phoneblock {
+	exports de.haumacher.phoneblock.app;
+
+	requires com.google.api.client;
+	requires com.google.auth;
+	requires com.google.auth.oauth2;
+	requires com.h2database;
+	requires com.opencsv;
+	requires ez.vcard;
+	requires java.desktop;
+	requires java.mail;
+	requires java.management;
+	requires java.naming;
+	requires java.sql;
+	requires java.xml;
+	requires javax.servlet.api;
+	requires msgbuf.api;
+	requires org.apache.httpcomponents.httpcore;
+	requires org.jsoup;
+	requires org.mybatis;
+	requires org.slf4j;
+}
