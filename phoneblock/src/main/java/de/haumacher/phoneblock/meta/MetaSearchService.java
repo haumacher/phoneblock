04347afe024f8f5d89329f60fde5b98fa8a8892b
Bernhard Haumacher <bhu@top-logic.com>
2023-04-08 11:00:59 +0200

    Wait for the next meta-search, only if a search was actually performed.

--- phoneblock/src/main/java/de/haumacher/phoneblock/meta/MetaSearchService.java
+++ phoneblock/src/main/java/de/haumacher/phoneblock/meta/MetaSearchService.java
@@ -6,34 +6,21 @@ package de.haumacher.phoneblock.meta;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.ServiceLoader;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.function.Supplier;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.haumacher.phoneblock.app.SearchServlet;
 import de.haumacher.phoneblock.crawl.FetchService;
-import de.haumacher.phoneblock.db.DB;
-import de.haumacher.phoneblock.db.DBService;
-import de.haumacher.phoneblock.db.SpamReports;
 import de.haumacher.phoneblock.db.model.UserComment;
 import de.haumacher.phoneblock.meta.plugins.AbstractMetaSearch;
 import de.haumacher.phoneblock.scheduler.SchedulerService;
@@ -57,7 +44,7 @@ public class MetaSearchService implements ServletContextListener, Runnable {
 	
 	private long _lastSearch = System.currentTimeMillis() - MIN_DELAY;
 	
-	private final ConcurrentLinkedQueue<Runnable> _jobs = new ConcurrentLinkedQueue<>();
+	private final ConcurrentLinkedQueue<Supplier<Boolean>> _jobs = new ConcurrentLinkedQueue<>();
 
 	/** 
 	 * Creates a {@link MetaSearchService}.
@@ -124,42 +111,11 @@ public class MetaSearchService implements ServletContextListener, Runnable {
 		}
 	}
 
-	/**
-	 * Main for debugging only.
-	 */
-	public List<UserComment> search(String phone) {
-		Stream<Pair<AbstractMetaSearch, Future<List<UserComment>>>> s1 = _plugins.stream().map(query -> Pair.of(query, _scheduler.executor().submit(() -> {
-			try {
-				return query.fetchComments(phone);
-			} catch (Throwable ex) {
-				return Collections.emptyList();
-			}
-		})));
-		
-		List<UserComment> comments = s1.flatMap(f -> {
-			try {
-				return f.getRight().get(5, TimeUnit.SECONDS).stream();
-			} catch (InterruptedException | ExecutionException ex) {
-				ex.printStackTrace();
-				return Collections.<UserComment>emptyList().stream();
-			} catch (TimeoutException ex) {
-				LOG.info("Timeout waiting for meta search: " + f.getLeft().getClass().getName());
-				return Collections.<UserComment>emptyList().stream();
-			}
-		})
-		.collect(Collectors.toList());
-		
-		return comments;
-	}
-	
 	/** 
 	 * Look up user comments for the given phone number.
 	 */
 	public List<UserComment> fetchComments(String phoneId) {
-		long lastVote = Long.MIN_VALUE;
-		int votes = 0;
-		
-		return doMetaSearch(votes, lastVote, phoneId);
+		return createSearch(phoneId).search().getComments();
 	}
 	
 	/**
@@ -174,7 +130,7 @@ public class MetaSearchService implements ServletContextListener, Runnable {
 	 *        The phone number to search comments and votes for.
 	 */
 	public void scheduleMetaSearch(int votes, long lastVote, String phoneId) {
-		_jobs.add(() -> doMetaSearch(votes, lastVote, phoneId));
+		_jobs.add(() -> createSearch(phoneId).addVotes(votes, lastVote).search().searchPerformed());
 
 		synchronized (this) {
 			reSchedule();
@@ -189,20 +145,23 @@ public class MetaSearchService implements ServletContextListener, Runnable {
 
 	@Override
 	public void run() {
-		Runnable job;
+		Supplier<Boolean> job;
 		long now;
 
-		synchronized (this) {
-			job = _jobs.poll();
-			if (job == null) {
-				return;
+		boolean searchPerformed;
+		do {
+			synchronized (this) {
+				job = _jobs.poll();
+				if (job == null) {
+					return;
+				}
+				
+				now = System.currentTimeMillis();
+				_lastSearch = now;
 			}
 			
-			now = System.currentTimeMillis();
-			_lastSearch = now;
-		}
-		
-		job.run();
+			searchPerformed = job.get();
+		} while (!searchPerformed);
 
 		synchronized (this) {
 			if (!_jobs.isEmpty()) {
@@ -222,79 +181,11 @@ public class MetaSearchService implements ServletContextListener, Runnable {
 		_task = _scheduler.executor().schedule(this, delay, TimeUnit.MILLISECONDS);
 	}
 
-	/**
-	 * Perform a meta search and store found comments and votes into the local DB.
-	 *
-	 * @param votes
-	 *        The votes to add to the given number (from some other source).
-	 * @param lastVote
-	 *        The time of the given votes ({@link Long#MIN_VALUE}, if no votes are given).
-	 * @param phoneId
-	 *        The phone number to search for.
-	 * @return All user comments that exist for the given phone number.
+	/** 
+	 * Creates a search for the given phone number.
 	 */
-	private List<UserComment> doMetaSearch(int votes, long lastVote, String phoneId) {
-		List<UserComment> comments;
-		DB db = DBService.getInstance();
-		try (SqlSession session = db.openSession()) {
-			SpamReports mapper = session.getMapper(SpamReports.class);
-
-			comments = new ArrayList<>(mapper.getComments(phoneId));
-
-			boolean commit = false;
-			long now = System.currentTimeMillis();
-			Long lastMetaSearch = mapper.getLastMetaSearch(phoneId);
-			boolean doSearch;
-			if (lastMetaSearch == null) {
-				mapper.insertLastMetaSearch(phoneId, now);
-				commit = true;
-				doSearch = true;
-			} else if (lastMetaSearch.longValue() < now - SearchServlet.ONE_MONTH) {
-				mapper.setLastMetaSearch(phoneId, now);
-				doSearch = true;
-				commit = true;
-			} else {
-				doSearch = false;
-			}
-			
-			boolean updatedRequired = false;
-			if (doSearch) {
-				LOG.info("Performing meta search for: " + phoneId);
-
-				Map<String, UserComment> indexedComments = comments.stream().collect(Collectors.toMap(c -> c.getComment(), c -> c));
-
-				int commentCnt = 0;
-				List<UserComment> newComments = search(phoneId);
-				for (UserComment comment : newComments) {
-					if (!indexedComments.containsKey(comment.getComment())) {
-						comment.setId(UUID.randomUUID().toString());
-						comments.add(comment);
-						
-						updatedRequired |= db.addRating(mapper, comment);
-						commentCnt++;
-						
-						commit = true;
-					}
-				}
-				LOG.info("Found " + commentCnt + " new comments: " + phoneId);
-			} else {
-				LOG.info("Skipping search, still up-to-date: " + phoneId);
-			}
-			
-			if (votes != 0) {
-				updatedRequired |= db.processVotes(mapper, phoneId, votes, lastVote);
-				commit = true;
-			}
-			
-			if (commit) {
-				session.commit();
-			}
-			
-			if (updatedRequired) {
-				db.publishUpdate(phoneId);
-			}
-		}
-		return comments;
+	private SearchOperation createSearch(String phoneId) {
+		return new SearchOperation(_scheduler, _plugins, phoneId);
 	}
 
 	/** 
@@ -312,7 +203,7 @@ public class MetaSearchService implements ServletContextListener, Runnable {
 		MetaSearchService searchService = new MetaSearchService(scheduler, new FetchService());
 		scheduler.contextInitialized(null);
 		searchService.contextInitialized(null);
-		List<UserComment> comments = searchService.search(args[0]);
+		List<UserComment> comments = searchService.createSearch(args[0]).search().getComments();
 		for (var comment : comments) {
 			System.out.println(comment);
 		}
---
+++ phoneblock/src/main/java/de/haumacher/phoneblock/meta/SearchOperation.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) 2023 Bernhard Haumacher et al. All Rights Reserved.
+ */
+package de.haumacher.phoneblock.meta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.haumacher.phoneblock.app.SearchServlet;
+import de.haumacher.phoneblock.db.DB;
+import de.haumacher.phoneblock.db.DBService;
+import de.haumacher.phoneblock.db.SpamReports;
+import de.haumacher.phoneblock.db.model.UserComment;
+import de.haumacher.phoneblock.meta.plugins.AbstractMetaSearch;
+import de.haumacher.phoneblock.scheduler.SchedulerService;
+
+/**
+ * A meta web search operation for a phone number.
+ */
+public class SearchOperation {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SearchOperation.class);
+
+	private int _votes;
+	private long _lastVote;
+	private String _phoneId;
+
+	private List<AbstractMetaSearch> _plugins;
+
+	private SchedulerService _scheduler;
+
+	private List<UserComment> _comments;
+
+	private boolean _searchPerformed;
+
+	/** 
+	 * Creates a {@link SearchOperation}.
+	 */
+	public SearchOperation(SchedulerService scheduler, List<AbstractMetaSearch> plugins, String phoneId) {
+		_scheduler = scheduler;
+		_plugins = plugins;
+		_phoneId = phoneId;
+	}
+
+	/** 
+	 * Performs the search and updates the local DB.
+	 */
+	public SearchOperation search() {
+		DB db = DBService.getInstance();
+		try (SqlSession session = db.openSession()) {
+			SpamReports mapper = session.getMapper(SpamReports.class);
+
+			_comments = new ArrayList<>(mapper.getComments(_phoneId));
+
+			_searchPerformed = shouldSearch(mapper, _phoneId);
+			
+			boolean indexUpdated = false;
+			if (_searchPerformed) {
+				LOG.info("Performing meta search for: " + _phoneId);
+
+				Map<String, UserComment> indexedComments = _comments.stream().collect(Collectors.toMap(c -> c.getComment(), c -> c));
+
+				int commentCnt = 0;
+				List<UserComment> newComments = doSearch();
+				for (UserComment comment : newComments) {
+					if (!indexedComments.containsKey(comment.getComment())) {
+						comment.setId(UUID.randomUUID().toString());
+						_comments.add(comment);
+						
+						db.addRating(mapper, comment);
+						commentCnt++;
+						indexUpdated = true;
+					}
+				}
+				LOG.info("Found " + commentCnt + " new comments: " + _phoneId);
+			} else {
+				LOG.info("Skipping search, still up-to-date: " + _phoneId);
+			}
+			
+			boolean commit = _searchPerformed;
+			
+			if (_votes != 0) {
+				indexUpdated |= db.processVotes(mapper, _phoneId, _votes, _lastVote);
+				commit = true;
+			}
+			
+			if (commit) {
+				session.commit();
+			}
+			
+			if (indexUpdated) {
+				db.publishUpdate(_phoneId);
+			}
+		}
+		return this;
+	}
+
+	/**
+	 * Whether the last {@link #search()} actually did update the stored comments. 
+	 */
+	public boolean searchPerformed() {
+		return _searchPerformed;
+	}
+
+	/**
+	 * Check whether an update of the meta-search results is required.
+	 * 
+	 * @return Whether a search should be performed.
+	 */
+	private boolean shouldSearch(SpamReports db, String phoneId) {
+		long now = System.currentTimeMillis();
+		Long lastMetaSearch = db.getLastMetaSearch(phoneId);
+		boolean doSearch;
+		if (lastMetaSearch == null) {
+			db.insertLastMetaSearch(phoneId, now);
+			doSearch = true;
+		} else if (lastMetaSearch.longValue() < now - SearchServlet.ONE_MONTH) {
+			db.setLastMetaSearch(phoneId, now);
+			doSearch = true;
+		} else {
+			doSearch = false;
+		}
+		return doSearch;
+	}
+
+	/**
+	 * Main for debugging only.
+	 */
+	private List<UserComment> doSearch() {
+		Stream<Pair<AbstractMetaSearch, Future<List<UserComment>>>> s1 = _plugins.stream().map(query -> Pair.of(query, _scheduler.executor().submit(() -> {
+			try {
+				return query.fetchComments(_phoneId);
+			} catch (Throwable ex) {
+				return Collections.emptyList();
+			}
+		})));
+		
+		List<UserComment> comments = s1.flatMap(f -> {
+			try {
+				return f.getRight().get(5, TimeUnit.SECONDS).stream();
+			} catch (InterruptedException | ExecutionException ex) {
+				ex.printStackTrace();
+				return Collections.<UserComment>emptyList().stream();
+			} catch (TimeoutException ex) {
+				LOG.info("Timeout waiting for meta search: " + f.getLeft().getClass().getName());
+				return Collections.<UserComment>emptyList().stream();
+			}
+		})
+		.collect(Collectors.toList());
+		
+		return comments;
+	}
+
+	/** 
+	 * All comments found in the local DB and during the meta search.
+	 */
+	public List<UserComment> getComments() {
+		return _comments;
+	}
+
+	/** 
+	 * Adds additional votes that should be stored in the local DB together with the search results.
+	 */
+	public SearchOperation addVotes(int votes, long lastVote) {
+		_votes = votes;
+		_lastVote = lastVote;
+		return this;
+	}
+	
+}
