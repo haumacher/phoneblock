393b8cc978ecf700dca926d889232dc501b6e75a
Bernhard Haumacher <haui@haumacher.de>
2023-10-30 19:53:13 +0100

    Use new file per recording.

--- phoneblock-ab/src/main/java/org/mjsip/ua/answerbot/AnswerBot.java
+++ phoneblock-ab/src/main/java/org/mjsip/ua/answerbot/AnswerBot.java
@@ -21,44 +21,20 @@
 package org.mjsip.ua.answerbot;
 
 import java.io.File;
-import java.io.IOError;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-import org.mjsip.media.AudioStreamer;
-import org.mjsip.media.FlowSpec;
 import org.mjsip.media.MediaDesc;
-import org.mjsip.media.MediaStreamer;
-import org.mjsip.media.RtpStreamReceiver;
-import org.mjsip.media.RtpStreamReceiverListener;
-import org.mjsip.media.RtpStreamReceiverListenerAdapter;
-import org.mjsip.media.RtpStreamSender;
-import org.mjsip.media.RtpStreamSenderListener;
-import org.mjsip.media.StreamerOptions;
-import org.mjsip.media.rx.AudioReceiver;
-import org.mjsip.media.rx.AudioRxHandle;
-import org.mjsip.media.rx.RtpAudioRxHandler;
-import org.mjsip.media.rx.RtpReceiverOptions;
-import org.mjsip.media.tx.AudioTXHandle;
-import org.mjsip.media.tx.AudioTransmitter;
-import org.mjsip.media.tx.RtpAudioTxHandle;
-import org.mjsip.media.tx.RtpSenderOptions;
-import org.mjsip.rtp.RtpControl;
-import org.mjsip.rtp.RtpPayloadFormat;
 import org.mjsip.sip.address.NameAddress;
 import org.mjsip.sip.call.RegistrationOptions;
 import org.mjsip.sip.message.SipMessage;
 import org.mjsip.sip.provider.SipConfig;
 import org.mjsip.sip.provider.SipProvider;
 import org.mjsip.sip.provider.SipStack;
-import org.mjsip.sound.AudioFile;
 import org.mjsip.time.Scheduler;
 import org.mjsip.time.SchedulerConfig;
 import org.mjsip.ua.MediaAgent;
@@ -73,13 +49,8 @@ import org.mjsip.ua.UserAgentListener;
 import org.mjsip.ua.UserAgentListenerAdapter;
 import org.mjsip.ua.pool.PortConfig;
 import org.mjsip.ua.pool.PortPool;
-import org.mjsip.ua.sound.AlawSilenceTrimmer;
 import org.mjsip.ua.streamer.StreamerFactory;
 import org.slf4j.LoggerFactory;
-import org.zoolu.net.UdpSocket;
-import org.zoolu.sound.CodecType;
-import org.zoolu.sound.SimpleAudioSystem;
-import org.zoolu.util.Encoder;
 import org.zoolu.util.Flags;
 
 /**
@@ -88,22 +59,19 @@ import org.zoolu.util.Flags;
  */
 public class AnswerBot extends MultipleUAS {
 	
-	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AnswerBot.class);
-
-	/** Media file to play when answering the call. */
-	public static String DEFAULT_ANNOUNCEMENT_FILE="./announcement-8000hz-mono-a-law.wav";
+	static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AnswerBot.class);
 
 	private MediaConfig _mediaConfig;
 
-	private final StreamerFactory _streamerFactory;
+	private Map<String, List<File>> _audioFragments;
 
 	/** 
 	 * Creates an {@link AnswerBot}. 
 	 */
-	public AnswerBot(SipProvider sip_provider, StreamerFactory streamerFactory, RegistrationOptions regOptions,
+	public AnswerBot(SipProvider sip_provider, Map<String, List<File>> audioFragments, RegistrationOptions regOptions,
 			UAOptions uaConfig, MediaConfig mediaConfig, PortPool portPool, ServiceOptions serviceConfig) {
 		super(sip_provider,portPool, regOptions, uaConfig, serviceConfig);
-		_streamerFactory = streamerFactory;
+		_audioFragments = audioFragments;
 		_mediaConfig = mediaConfig;
 	}
 	
@@ -112,8 +80,23 @@ public class AnswerBot extends MultipleUAS {
 		return new UserAgentListenerAdapter() {
 			@Override
 			public void onUaIncomingCall(UserAgent ua, NameAddress callee, NameAddress caller, MediaDesc[] media_descs) {
+				String callId = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss-SSS").format(new Date()) + " " + name(caller);
+				StreamerFactory streamerFactory = new DialogueFactory(_audioFragments, callId);
+				
 				LOG.info("Incomming call from: " + callee.getAddress());
-				ua.accept(new MediaAgent(_mediaConfig.getMediaDescs(), _streamerFactory));
+				ua.accept(new MediaAgent(_mediaConfig.getMediaDescs(), streamerFactory));
+			}
+
+			private String name(NameAddress caller) {
+				return afterAt(caller.getAddress().getSpecificPart());
+			}
+
+			private String afterAt(String specificPart) {
+				int atIndex = specificPart.indexOf('@');
+				if (atIndex < 0) {
+					return specificPart;
+				}
+				return specificPart.substring(0, atIndex);
 			}
 		};
 	}
@@ -148,66 +131,8 @@ public class AnswerBot extends MultipleUAS {
 			}
 		}
 		
-		StreamerFactory streamerFactory = new StreamerFactory() {
-			@Override
-			public MediaStreamer createMediaStreamer(FlowSpec flow_spec) {
-				SpeechDispatcher speechDispatcher = new SpeechDispatcher(audioFragments);
-
-				int sampleRate = flow_spec.getMediaSpec().getSampleRate();
-				try {
-					OutputStream recording = AudioFile.getAudioFileOutputStream("recording.wav", SimpleAudioSystem.getAudioFormat(flow_spec.getMediaSpec().getCodecType(), sampleRate));
-							
-					int bufferTime = 20;
-					int minSilenceTime = 500;
-					int paddingTime = 100;
-					double silenceDb = -30;
-					AlawSilenceTrimmer silenceTrimmer = new AlawSilenceTrimmer(sampleRate, bufferTime, minSilenceTime, paddingTime, silenceDb, recording, speechDispatcher);
-					
-					AudioTransmitter tx = new AudioTransmitter() {
-						@Override
-						public AudioTXHandle createSender(RtpSenderOptions options, UdpSocket udp_socket, AudioFormat audio_format,
-								CodecType codec, int payload_type, RtpPayloadFormat payloadFormat, int sample_rate, int channels,
-								Encoder additional_encoder, long packet_time, int packet_size, String remote_addr, int remote_port,
-								RtpStreamSenderListener listener, RtpControl rtpControl) throws IOException {
-							
-							RtpStreamSender sender = new RtpStreamSender(options, speechDispatcher, true, payload_type, payloadFormat, sample_rate,
-									channels, packet_time, packet_size, additional_encoder, udp_socket, remote_addr, remote_port, rtpControl, listener);
-							return new RtpAudioTxHandle(sender);
-						}
-					};
-					
-					AudioReceiver rx = new AudioReceiver() {
-						@Override
-						public AudioRxHandle createReceiver(RtpReceiverOptions options, UdpSocket socket,
-								AudioFormat audio_format, CodecType codec, int payload_type, RtpPayloadFormat payloadFormat,
-								int sample_rate, int channels, Encoder additional_decoder,
-								RtpStreamReceiverListener listener) throws IOException, UnsupportedAudioFileException {
-							
-							RtpStreamReceiverListener onTerminate = new RtpStreamReceiverListenerAdapter() {
-								@Override
-								public void onRtpStreamReceiverTerminated(RtpStreamReceiver rr, Exception error) {
-									try {
-										silenceTrimmer.close();
-										recording.close();
-									} catch (IOException ex) {
-										LOG.error("Failed to close recording.", ex);
-									}
-								}
-							}.andThen(listener);
-							
-							return new RtpAudioRxHandler(new RtpStreamReceiver(options, silenceTrimmer, additional_decoder, payloadFormat, socket, onTerminate));
-						}
-					};
-					StreamerOptions options = StreamerOptions.builder().build();
-					return new AudioStreamer(flow_spec, tx, rx, options);
-				} catch (IOException | UnsupportedAudioFileException ex) {
-					throw new IOError(ex);
-				}
-			}
-		};
-		
 		SipProvider sipProvider = new SipProvider(sipConfig, new Scheduler(schedulerConfig));
-		new AnswerBot(sipProvider, streamerFactory, uaConfig, uaConfig, mediaConfig, portConfig.createPool(), serviceConfig);
+		new AnswerBot(sipProvider, audioFragments, uaConfig, uaConfig, mediaConfig, portConfig.createPool(), serviceConfig);
 	}    
 
 }
