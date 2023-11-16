7e14cba0be8815816df0914845143d21ad823375
Bernhard Haumacher <haui@haumacher.de>
2023-11-16 18:01:48 +0100

    Adjusted to modularization of mjsip, introduced answer bot module.

---
+++ phoneblock-ab/src/main/java/module-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Bernhard Haumacher et al. All Rights Reserved.
+ */
+
+/**
+ * The PhoneBlock answer bot.
+ */
+module phoneblock.ab {
+	requires phoneblock.shared;
+
+	requires org.mjsip.sound;
+	requires org.mjsip.sip;
+	requires org.mjsip.ua;
+	requires org.mjsip.util; 
+	requires org.mjsip.net; 
+
+	requires args4j;
+	
+	requires java.base;
+	requires java.desktop;
+	requires de.haumacher.msgbuf;
+	requires org.slf4j;
+	requires org.tinylog.api;
+	
+	exports de.haumacher.phoneblock.answerbot;
+	exports de.haumacher.phoneblock.answerbot.tools;
+	
+	opens de.haumacher.phoneblock.answerbot.tools to args4j;
+}
