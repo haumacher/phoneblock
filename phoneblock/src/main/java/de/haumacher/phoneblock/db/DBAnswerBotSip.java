8b7d1acd2f051565b69a2f41330aa39e40bd0cea
Bernhard Haumacher <haui@haumacher.de>
2023-11-27 22:25:58 +0100

    Added task updating answerbot registrations upon change.

--- phoneblock/src/main/java/de/haumacher/phoneblock/db/DBAnswerBotSip.java
+++ phoneblock/src/main/java/de/haumacher/phoneblock/db/DBAnswerBotSip.java
@@ -6,18 +6,18 @@ package de.haumacher.phoneblock.db;
 import de.haumacher.phoneblock.db.settings.AnswerBotSip;
 
 /**
- * TODO
- *
- * @author <a href="mailto:haui@haumacher.de">Bernhard Haumacher</a>
+ * Record representing a single answer bot.
  */
 public class DBAnswerBotSip extends AnswerBotSip {
 	
 	/** 
 	 * Creates a {@link DBAnswerBotSip}.
 	 */
-	public DBAnswerBotSip(long userId, String host, String registrar, String realm, String userName, String passwd) {
+	public DBAnswerBotSip(long userId, String host, String ip4, String ip6, String registrar, String realm, String userName, String passwd) {
 		setUserId(userId);
 		setHost(host);
+		setIpv4(ip4);
+		setIpv6(ip6);
 		setRegistrar(registrar);
 		setRealm(realm);
 		setUserName(userName);
