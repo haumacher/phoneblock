b112b6e30e26e1f78a711965e826504361378804
Bernhard Haumacher <haui@haumacher.de>
2024-02-04 17:48:11 +0100

    Round talk time to full seconds.

--- phoneblock_answerbot_ui/lib/AnswerBotList.dart
+++ phoneblock_answerbot_ui/lib/AnswerBotList.dart
@@ -137,7 +137,7 @@ class AnswerBotListState extends State<AnswerBotList> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('Anrufbeantworter ${bot.userName}', overflow: TextOverflow.ellipsis, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),),
-                      Text('${bot.callsAccepted} Anrufe, ${bot.talkTime / 1000} s Gesprächszeit'),
+                      Text('${bot.callsAccepted} Anrufe, ${(bot.talkTime / 1000).round()} s Gesprächszeit'),
                     ],
                   ),
                 ),
