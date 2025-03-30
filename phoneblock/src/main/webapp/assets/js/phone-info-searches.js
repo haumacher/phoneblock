a8f7c25c99a0970dc112ca5092e60e3682b12dac
Bernhard Haumacher <haui@haumacher.de>
2025-03-30 18:36:15 +0200

    Fixed chart display.

--- phoneblock/src/main/webapp/assets/js/phone-info-searches.js
+++ phoneblock/src/main/webapp/assets/js/phone-info-searches.js
@@ -1,29 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const chartDataElement = document.getElementById('searches-data');
 
-    // get data
-    const labels = chartDataElement.getAttribute('data-labels').split(",");
-    const dataset = chartDataElement.getAttribute('data-data').split(",");
+	const data = JSON.parse(chartDataElement.textContent);
 
     // make chart
-    new Chart(document.getElementById('searches').getContext('2d'), {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Suchanfragen in der letzten Woche',
-                data: dataset,
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+    new Chart(document.getElementById('searches').getContext('2d'), data);
 });
