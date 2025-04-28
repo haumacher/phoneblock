09f9dd68740385525378fcff98b424a39953146a
Bernhard Haumacher <haui@haumacher.de>
2025-04-28 21:13:29 +0200

    Display phone numbers in international format.
    On status page and in personal settings.

--- phoneblock/src/main/java/de/haumacher/phoneblock/app/render/Converters.java
+++ phoneblock/src/main/java/de/haumacher/phoneblock/app/render/Converters.java
@@ -2,6 +2,8 @@ package de.haumacher.phoneblock.app.render;
 
 import java.util.Date;
 
+import de.haumacher.phoneblock.analysis.NumberAnalyzer;
+import de.haumacher.phoneblock.app.api.model.PhoneNumer;
 import de.haumacher.phoneblock.app.api.model.Rating;
 
 public class Converters {
@@ -18,5 +20,9 @@ public class Converters {
 	
 	public boolean isPositive(Rating rating) {
 		return rating == Rating.A_LEGITIMATE;
+	}
+	
+	public PhoneNumer analyze(String phoneId) {
+		return NumberAnalyzer.analyze(phoneId);
 	}
 }
