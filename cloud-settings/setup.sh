b54ffe43094ebb2461a5042ccd14c287e6b05483
Bernhard Haumacher <haui@haumacher.de>
2025-06-14 21:46:07 +0200

    Fixed domain name.

--- cloud-settings/setup.sh
+++ cloud-settings/setup.sh
@@ -2,5 +2,5 @@
 
 # Commands to trigger manually after the server and DNS is successfully set up.
 
-certbot --apache -n --agree-tos --email haui@haumacher.de --domains phoneblock2.haumacher.de
+certbot --apache -n --agree-tos --email haui@haumacher.de --domains phoneblock.haumacher.de
 
