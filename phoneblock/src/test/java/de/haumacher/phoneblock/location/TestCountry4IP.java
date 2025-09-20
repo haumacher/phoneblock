b30ea12e2d65f1d2209a82d8ebad65f7e9866f7a
Bernhard Haumacher <haui@haumacher.de>
2025-09-20 22:08:46 +0200

    Switched to truezip plugin, since antrun stopped working.

--- phoneblock/src/test/java/de/haumacher/phoneblock/location/TestCountry4IP.java
+++ phoneblock/src/test/java/de/haumacher/phoneblock/location/TestCountry4IP.java
@@ -2,12 +2,11 @@ package de.haumacher.phoneblock.location;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 
 import org.junit.jupiter.api.Test;
-
-import de.haumacher.phoneblock.location.Country4IP;
 
 /**
  * Test case for {@link Country4IP}.
@@ -16,7 +15,9 @@ public class TestCountry4IP {
 	
 	@Test
 	public void testLookup() throws IOException {
-		Country4IP country = new Country4IP("./target/IP2LOCATION/IP2LOCATION-LITE-DB1.BIN", "./target/IP2LOCATION/IP2LOCATION-LITE-DB1.IPV6.BIN");
+		Country4IP country = new Country4IP(
+			resolve("./target/IP2LOCATION/IP2LOCATION-LITE-DB1.BIN"), 
+			resolve("./target/IP2LOCATION/IP2LOCATION-LITE-DB1.IPV6.BIN"));
 		
         assertEquals("DE", country.lookup("phoneblock.net"));
         assertEquals("DE", country.lookup("128.140.84.131"));
@@ -24,6 +25,10 @@ public class TestCountry4IP {
         
         assertEquals("DE", country.lookup(InetAddress.getByName("128.140.84.131")));
         assertEquals("DE", country.lookup(InetAddress.getByName("2a01:4f8:c17:6624::1")));
+	}
+
+	private String resolve(String fileName) {
+		return new File(fileName).getAbsolutePath();
 	}
 	
 }
